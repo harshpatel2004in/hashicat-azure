f8ec1a9ca4f764663fe1639294eb675b1a857a0a
Harsh Patel <harshpatel2004in@yahoo.com>
2022-09-15 09:12:42 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,6 +2,5 @@ module "network" {
   source  = "app.terraform.io/harsh-learns-tf/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "myresourcegroup" {
-  name     = "${var.prefix}-workshop"
+  resource_group_name = "${var.prefix}-workshop"
 }
